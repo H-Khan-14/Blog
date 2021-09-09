5851fe90fdd97e42790d0fa6d9b9508732ded79a
H-Khan-14 <haseebkhan14@hotmail.com>
2021-09-09 17:32:01 -0400

    Publish to GitHub Pages and hopefully change to index.js works

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,8 @@ import postReducers from './postsReducer';
 import usersReducer from './usersReducer';
 
 export default combineReducers({
-  /*dummyReducer: () =>
+  /*dummyReducer: () => 14
     'For beginners who do not yet know how to structure their reducers a dummy reducer works like this. It makes the error go away so you can continue working.'*/
   posts: postReducers,
-  users: usersReducer
+  users: usersReducer,
 });
